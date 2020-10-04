7cf1408018add333a48b8c3c780dec232377aae7
Libia Sana <lifasan@hotmail.com>
2020-10-04 16:13:53 +0200

    estoy enviendo nuevamente todo

---
+++ docs/precache-manifest.6380c0f48ad0ab6c902872fc99d42404.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad1bca6265dc7975a981207d78d5b3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f7d73304ed2c12d5974",
+    "url": "/static/css/main.d722253c.chunk.css"
+  },
+  {
+    "revision": "c891655c70870ca4f1e5",
+    "url": "/static/js/2.00eec8fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00eec8fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f7d73304ed2c12d5974",
+    "url": "/static/js/main.acccbc87.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
